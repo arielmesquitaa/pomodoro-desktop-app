d54e6803c484502cef686a1367f3dfcd11611e88
Ariel Mesquita <arielmesquita@live.com>
2022-10-08 12:30:58 -0300

    Setup UI

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#DD5353"
-RED = "#B73E3E"
+from tkinter import Canvas, PhotoImage, Tk
+
+PINK = "##e2979c"
+RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN = 10
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
+canvas.pack()
+
+window.mainloop()
